fe108714a9c8f8a8a6b61f89c2af76f270201dc0
Carlos Mourullo <carlosmourullo@gmail.com>
2016-08-14 17:07:21 +0100

    create the class PostService

---
+++ module/Blog/src/Blog/Service/PostService.php
@@ -0,0 +1,21 @@
+<?php
+namespace Blog\Service;
+
+class PostService implements PostServiceInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function findAllPosts()
+    {
+        // TODO: Implement findAllPosts() method.
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findPost($id)
+    {
+        // TODO: Implement findPost() method.
+    }
+}
