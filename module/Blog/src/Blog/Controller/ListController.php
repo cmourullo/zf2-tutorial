fed6a6ccf25f8a86a59af9b5c9459ee3f3e29a73
Carlos Mourullo <carlosmourullo@gmail.com>
2016-08-14 17:55:23 +0100

    overwrite default index

--- module/Blog/src/Blog/Controller/ListController.php
+++ module/Blog/src/Blog/Controller/ListController.php
@@ -3,6 +3,7 @@ namespace Blog\Controller;
 
 use Blog\Service\PostServiceInterface;
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
 
 class ListController extends AbstractActionController
 {
@@ -14,5 +15,12 @@ class ListController extends AbstractActionController
     public function __construct(PostServiceInterface $postService)
     {
         $this->postService = $postService;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel(array(
+            'posts' => $this->postService->findAllPosts()
+        ));
     }
 }
