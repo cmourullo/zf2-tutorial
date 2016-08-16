3390d07ec7a3ea39d1e48343374e81e01f669091
Carlos Mourullo <carlosmourullo@gmail.com>
2016-08-16 07:57:06 +0100

    create contructor for the service

--- module/Blog/src/Blog/Service/PostService.php
+++ module/Blog/src/Blog/Service/PostService.php
@@ -5,33 +5,18 @@ use Blog\Model\Post;
 
 class PostService implements PostServiceInterface
 {
-    protected $data = array(
-        array(
-            'id'    => 1,
-            'title' => 'Hello World #1',
-            'text'  => 'This is our first blog post!'
-        ),
-        array(
-            'id'     => 2,
-            'title' => 'Hello World #2',
-            'text'  => 'This is our second blog post!'
-        ),
-        array(
-            'id'     => 3,
-            'title' => 'Hello World #3',
-            'text'  => 'This is our third blog post!'
-        ),
-        array(
-            'id'     => 4,
-            'title' => 'Hello World #4',
-            'text'  => 'This is our fourth blog post!'
-        ),
-        array(
-            'id'     => 5,
-            'title' => 'Hello World #5',
-            'text'  => 'This is our fifth blog post!'
-        )
-    );
+    /**
+     * @var \Blog\Mapper\PostMapperInterface
+     */
+    protected $postMapper;
+
+    /**
+     * @param PostMapperInterface $postMapper
+     */
+    public function __construct(PostMapperInterface $postMapper)
+    {
+        $this->postMapper = $postMapper;
+    }
 
     /**
      * {@inheritDoc}
