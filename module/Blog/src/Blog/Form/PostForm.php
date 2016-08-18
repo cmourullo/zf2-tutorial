873b0ba1943c82553be64d3fc12504179c11e236
Carlos Mourullo <carlosmourullo@gmail.com>
2016-08-18 14:56:18 +0100

    create post form

---
+++ module/Blog/src/Blog/Form/PostForm.php
@@ -0,0 +1,24 @@
+<?php
+// Filename: /module/Blog/src/Blog/Form/PostForm.php
+namespace Blog\Form;
+
+use Zend\Form\Form;
+
+class PostForm extends Form
+{
+    public function __construct()
+    {
+        $this->add(array(
+            'name' => 'post-fieldset',
+            'type' => 'Blog\Form\PostFieldset'
+        ));
+
+        $this->add(array(
+            'type' => 'submit',
+            'name' => 'submit',
+            'attributes' => array(
+                'value' => 'Insert new Post'
+            )
+        ));
+    }
+}
