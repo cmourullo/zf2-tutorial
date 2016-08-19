76c632781a91512cecc3f001b95baf683647a553
Carlos Mourullo <carlosmourullo@gmail.com>
2016-08-19 10:56:18 +0100

    call form constructor

--- module/Blog/src/Blog/Form/PostForm.php
+++ module/Blog/src/Blog/Form/PostForm.php
@@ -8,6 +8,8 @@ class PostForm extends Form
 {
     public function __construct()
     {
+        parent::__construct($name, $options);
+        
         $this->add(array(
             'name' => 'post-fieldset',
             'type' => 'Blog\Form\PostFieldset'
